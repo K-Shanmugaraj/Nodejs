b6545f89ebf88b3a320939649db59bda1ce2d03b
K-Shanmugaraj <shanmugarajk2205@gmail.com>
2024-11-04 14:27:04 +0530

    Initial command

--- express.js
+++ express.js
@@ -1,28 +1,51 @@
-const express= require('express');
-const app=express();
+const express = require('express');
+const app = express();
+
+app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-// app.get("/",(req,res)=>
-//     {  let qry= req.query
-//         res.send(QuereyString : ${qry.id}, ${qry.name})
+
+const blog = require('./blogExpress');
+
+app.get("/getAllBlogPosts", blog.readAllBlogPosts);
+
+app.get("/getSpecificBlogPosts", blog.readSpecificBlogPost);
+
+app.post("/createBlogPost", blog.createBlogPost);
+
+app.put("/updateBlogPost", blog.updateBlogPost);
+
+app.use("/*", (req, res) => {
+    res.send("No routes match found!");
+});
+
+app.listen(4000, () => console.log('Server running on port : 4000'));
+
+
+// const express= require('express');
+// const app=express();
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+// // app.get("/",(req,res)=>
+// //     {  let qry= req.query
+// //         res.send(QuereyString : ${qry.id}, ${qry.name})
         
-//     });
-// app.post("/",(req,res)=>
-//         {  let qry= req.body
-//             res.send(URL ENCODED : ${qry.TITLE}, ${qry.DESC})
+// //     });
+// // app.post("/",(req,res)=>
+// //         {  let qry= req.body
+// //             res.send(URL ENCODED : ${qry.TITLE}, ${qry.DESC})
             
-//         });
-// app.put("/",(req,res)=>
-//             {  let qry= req.body//JSON
-//                 res.send(URL ENCODED : ${qry.TITLE}, ${qry.DESC})
+// //         });
+// // app.put("/",(req,res)=>
+// //             {  let qry= req.body//JSON
+// //                 res.send(URL ENCODED : ${qry.TITLE}, ${qry.DESC})
                 
-//             });
-const BlogRoutes= require("./routes/blogRoutes")
-app.use("/api/v1/blog",BlogRoutes)
+// //             });
+// const BlogRoutes= require("./routes/blogRoutes")
+// app.use("/api/v1/blog",BlogRoutes)
     
-// app.use("/*",(res)=>
-//     { 
-//         res.send("No routes found");
-//     });
+// // app.use("/*",(res)=>
+// //     { 
+// //         res.send("No routes found");
+// //     });
   
-app.listen(4000, () => console.log("Server has started in port: 4000"))
+// app.listen(4000, () => console.log("Server has started in port: 4000"))
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,13 +1,27 @@
-const router=require('express').Router()
-router.get("/readAllBlogPosts",(req,res)=>
-        {  let qry= req.query
-            res.send(`QuereyString : ${qry.id}, ${qry.name}`)
-            
-        });
-        router.post("/readAllBlogPosts",(req,res)=>
-        {  let qry= req.body
-            res.send(`URL ENCODED : ${qry.TITLE}, ${qry.DESC}`)
-            
-        });
+const router = require('express').Router();
 
-        module.exports = router;
+const BlogControllers = require('../controllers/blogControllers');
+
+router.get("/getAllBlogPosts", BlogControllers.getAllBlogPosts);
+router.get("/getSpecificBlogPosts", BlogControllers.getSpecificBlogPosts);
+router.post("/createBlogPost", BlogControllers.createBlogPost);
+router.put("/updateBlogPost", BlogControllers.updateBlogPost);
+router.delete("/deleteBlogPost/:id", BlogControllers.deleteBlogPost);
+
+module.exports = router;
+
+
+
+// const router=require('express').Router()
+// router.get("/readAllBlogPosts",(req,res)=>
+//         {  let qry= req.query
+//             res.send(`QuereyString : ${qry.id}, ${qry.name}`)
+            
+//         });
+//         router.post("/readAllBlogPosts",(req,res)=>
+//         {  let qry= req.body
+//             res.send(`URL ENCODED : ${qry.TITLE}, ${qry.DESC}`)
+            
+//         });
+
+//         module.exports = router;
